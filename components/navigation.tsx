c362879fec0226ccf86c35028fd8192730b0785d
“dogspecial” <dogspecial@naver.com>
2025-01-01 22:11:50 +0900

    feat: navigation use client 추가

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,15 +1,17 @@
-"use client";
-
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+    const path = usePathname();
+    console.log(path);
     return (
         <nav>
             <ul>
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/dashboard">Dashboard</Link></li>
-                <li><Link href="/member">Member</Link></li>
-                <li><Link href="/works">Works</Link></li>
+                <li><Link href="/">Home</Link>{path === '/' ? '♥️' : ''}</li>
+                <li><Link href="/dashboard">Dashboard</Link>{path === '/dashboard' ? '♥️' : ''}</li>
+                <li><Link href="/member">Member</Link>{path === '/member' ? '♥️' : ''}</li>
+                <li><Link href="/works">Works</Link>{path === '/works' ? '♥️' : ''}</li>
             </ul>
         </nav>
     )
